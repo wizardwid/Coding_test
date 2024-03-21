74f2bab1a5cab1b30b366c39c7b5fa0f37020c5b
wizardwid <141827919+wizardwid@users.noreply.github.com>
2024-03-21 22:19:52 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int a = 1;
+        int b = 0;
+        
+        for(int i=0; i<num_list.length; i++){
+            a *= num_list[i];
+            b += num_list[i];
+        }
+        
+        if(b*b > a){
+                answer = 1;
+            }
+        
+        else{
+            answer = 0;
+        }
+        
+        return answer;
+    }
+}
